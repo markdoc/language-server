c7bd73dcdf5b85deffcc1207263d34bf439f775b
Mike Fix <mfix@stripe.com>
2023-05-23 11:15:44 -0700

    Add release GH actions

---
+++ README.md
@@ -0,0 +1,23 @@
+# `@markdoc/language-server`
+
+> **Note**: this repo will be treated as a beta version until `1.0.0` is released.
+
+## Setup
+
+## Contributing
+
+Contributions and feedback are welcomed and encouraged. Feel free to open PRs here, or open issues in the [Markdoc core repo](https://github.com/markdoc/markdoc).
+
+Follow these steps to set up the project:
+
+1. Run `npm install`
+1. Run `npm build`
+1. Run `npm test`
+
+## Code of conduct
+
+This project has adopted the Stripe [Code of conduct](https://github.com/markdoc/markdoc/blob/main/.github/CODE_OF_CONDUCT.md).
+
+## License
+
+This project uses the [MIT license](LICENSE).
--- build.mjs
+++ build.mjs
@@ -1,16 +1,16 @@
-import { context, build } from "esbuild";
+import {context, build} from 'esbuild';
 
 const config = {
   bundle: true,
-  entryPoints: ["client/index.ts", "server/index.ts", "client/server.ts"],
-  outdir: "build",
-  sourcemap: "linked",
-  external: ["vscode"],
-  platform: "node",
-  format: "cjs",
+  entryPoints: ['client/index.ts', 'server/index.ts', 'client/server.ts'],
+  outdir: 'dist',
+  sourcemap: 'linked',
+  external: ['vscode'],
+  platform: 'node',
+  format: 'cjs',
 };
 
-if (process.argv.includes("--watch")) {
+if (process.argv.includes('--watch')) {
   const ctx = await context(config);
   await ctx.watch();
 } else build(config);
